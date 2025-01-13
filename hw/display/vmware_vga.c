dea4a3d97af81d89472b54e513cbb29f8be0656c
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2025-01-13 07:47:53 -0500

    Update vmware_vga.c

--- hw/display/vmware_vga.c
+++ hw/display/vmware_vga.c
@@ -4,7 +4,7 @@
   
   Copyright (c) 2007 Andrzej Zaborowski <balrog@zabor.org>
   
-  Copyright (c) 2023-2024 Christopher Eric Lentocha <christopherericlentocha@gmail.com>
+  Copyright (c) 2023-2025 Christopher Eric Lentocha <christopherericlentocha@gmail.com>
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
